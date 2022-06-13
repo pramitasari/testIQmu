3ab9cf87950f31a95326dda70fb9139b1517e550
pramitasari <105182828+pramitasari@users.noreply.github.com>
2022-06-13 13:13:13 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Selamat datang" />
+        <Header title="SELAMAT DATANG" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
